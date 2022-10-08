e979735879ab07a13da982c93728f89ac97d44cc
Gustavo Teixeira <contact@gustavoteixeira.dev>
2022-10-08 02:27:24 -0300

    trigger pipe

--- src/main/java/dev/gustavoteixeira/easygest/adapter/JwtProperties.java
+++ src/main/java/dev/gustavoteixeira/easygest/adapter/JwtProperties.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 @Data
 public class JwtProperties {
 
-    private String secretKey = "eyJhbGciOiJIUzI1NiJ9";
+    private String secretKey = "rzxlszyykpbgqcflzxsqcysyhljt";
 
     // validity in milliseconds
     private long validityInMs = 3600000 * 24 * 7; // 1h
